9e5812c4b2effd914ff793132ad3cf3c34f4df4e
ian5258 <ian@axolotl.com.tw>
2021-05-13 13:40:22 +0800

    [add] remark

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func main() {
 		chromedp.ExecPath("/Users/ian/Desktop/Chromium.app/Contents/MacOS/Chromium"),
 		chromedp.Flag("no-default-browser-check", true),
 		chromedp.Flag("no-sandbox", true),
-		chromedp.Flag("blink-settings", "imagesEnabled=false"),
-		chromedp.Flag("headless", true),
+		chromedp.Flag("blink-settings", "imagesEnabled=false"), // 不加載圖片
+		chromedp.Flag("headless", true),                        // 無頭模式
 		chromedp.WindowSize(1920, 1080),
 		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
 	}
@@ -47,19 +47,20 @@ func main() {
 	)
 	defer cancel()
 
-	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 40*time.Second)
+	//建立一個上下文，超時時間為40s
+	ctx, cancel = context.WithTimeout(ctx, 300*time.Second)
 	defer cancel()
 
-	Rank := 0
+	Rank := 0 // 排名
 	hasCurrentItem := false
+	keyWord := "二手精品" // 搜尋關鍵字
 
 	for i := 1; i <= 20; i++ {
-		fmt.Printf("%d\n", i)
+		fmt.Printf("now in Page: %d\n", i)
 		// navigate
 		if i == 1 {
 			err = chromedp.Run(ctx,
-				chromedp.Navigate(`https://www.google.com.tw/search?q=二手精品`),
+				chromedp.Navigate("https://www.google.com.tw/search?q="+keyWord),
 				chromedp.Sleep(1*time.Second),
 				chromedp.WaitVisible(`#search div[id="rso"]`),
 			)
@@ -95,35 +96,6 @@ func main() {
 			return
 		}
 	}
-
-	// navigate to a page, wait for an element, click
-	// var htmlContent string
-	// runErr := chromedp.Run(ctx,
-	// 	// chromedp.Navigate(`https://www.google.com.tw/search?q=台中二手精品`),
-	// 	chromedp.Navigate(`https://www.google.com.tw`),
-	// 	chromedp.WaitVisible(`.SDkEP input[name="q"]`, chromedp.BySearch), // waiting for element exist
-
-	// 	chromedp.Sleep(1*time.Second),
-	// 	chromedp.SetValue(`.SDkEP input[name="q"]`, "台中二手精品", chromedp.BySearch),
-	// 	chromedp.SendKeys(`.SDkEP input[name="q"]`, kb.Enter), // 按下Enter
-	// 	chromedp.WaitVisible(`#search div[id="rso"]`),
-	// 	chromedp.OuterHTML(`#search div[id="rso"]`, &htmlContent, chromedp.BySearch),
-	// )
-	// htmlContent, runErr := outerPageSourceData(ctx)
-	// if runErr != nil {
-	// 	logger.Info("Run err : %v\n", runErr)
-	// 	return
-	// }
-
-	// sleepErr := chromedp.Run(ctx,
-	// 	chromedp.Sleep(10*time.Second),
-	// )
-	// if sleepErr != nil {
-	// 	log.Fatal(sleepErr)
-	// }
-	// // log.Printf("OK\n")
-	// // log.Printf("：" + htmlContent)
-	// ParsingData(htmlContent)
 }
 
 // 輸出page source data
@@ -137,6 +109,7 @@ func outerPageSourceData(ctx context.Context) (string, error) {
 	return htmlContent, err
 }
 
+// 解析資料
 func ParsingData(res string, rank int, page int) (int, bool) {
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader([]byte(res)))
 	if err != nil {
@@ -147,50 +120,15 @@ func ParsingData(res string, rank int, page int) (int, bool) {
 	hasCurrentItem := false
 	doc.Find(".yuRUbf").Each(func(i int, s *goquery.Selection) {
 		itemTitle = s.Find(".LC20lb").Text()
-		isCurrentItem := strings.Contains(itemTitle, "Relithe")
+		isCurrentItem := strings.Contains(itemTitle, "Relithe") // 模糊搜尋是否含有關鍵字
 		rank++
 		if isCurrentItem {
 			hasCurrentItem = true
-			fmt.Printf("\n Rank：%d,\n Page：%d,\n Index：%d,\n Title：%s\n", rank, page, (i + 1), itemTitle)
+			fmt.Print("\n")
+			fmt.Printf("Rank：%d,\nPage：%d,\nIndex：%d,\nTitle：%s\n", rank, page, (i + 1), itemTitle)
 		}
 		time.Sleep(1 * time.Second)
 	})
 
 	return rank, hasCurrentItem
 }
-
-// //獲取網站上爬取的資料
-// func GetHttpHtmlContent(url string, selector string, sel interface{}) (string, error) {
-// 	options := []chromedp.ExecAllocatorOption{
-// 		chromedp.Flag("headless", true), // debug使用
-// 		chromedp.Flag("blink-settings", "imagesEnabled=false"),
-// 		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
-// 	}
-// 	//初始化引數，先傳一個空的資料
-// 	options = append(chromedp.DefaultExecAllocatorOptions[:], options...)
-
-// 	c, _ := chromedp.NewExecAllocator(context.Background(), options...)
-
-// 	// create context
-// 	chromeCtx, cancel := chromedp.NewContext(c, chromedp.WithLogf(log.Printf))
-// 	// 執行一個空task, 用提前建立Chrome例項
-// 	chromedp.Run(chromeCtx, make([]chromedp.Action, 0, 1)...)
-
-// 	//建立一個上下文，超時時間為40s
-// 	timeoutCtx, cancel := context.WithTimeout(chromeCtx, 40*time.Second)
-// 	defer cancel()
-
-// 	var htmlContent string
-// 	err := chromedp.Run(timeoutCtx,
-// 		chromedp.Navigate(url),
-// 		chromedp.WaitVisible(selector),
-// 		chromedp.OuterHTML(sel, &htmlContent, chromedp.ByJSPath),
-// 	)
-// 	if err != nil {
-// 		logger.Info("Run err : %v\n", err)
-// 		return "", err
-// 	}
-// 	//log.Println(htmlContent)
-
-// 	return htmlContent, nil
-// }
