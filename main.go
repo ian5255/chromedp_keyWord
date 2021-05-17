ddc7ac09097f49e056bf354645e876ac83f149e5
ian5258 <ian@axolotl.com.tw>
2021-05-17 16:28:37 +0800

    backup

--- main.go
+++ main.go
@@ -18,63 +18,9 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-func main() {
-	options := []chromedp.ExecAllocatorOption{
-		chromedp.ExecPath("/Users/ian/Desktop/Chromium.app/Contents/MacOS/Chromium"),
-		chromedp.Flag("no-default-browser-check", true),
-		chromedp.Flag("no-sandbox", true),
-		chromedp.Flag("blink-settings", "imagesEnabled=false"), // 不加載圖片
-		chromedp.Flag("headless", true),                        // 無頭模式
-		chromedp.WindowSize(1920, 1080),
-		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
-	}
-
-	dir, err := ioutil.TempDir("", "chromedp-example")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	options = append(chromedp.DefaultExecAllocatorOptions[:], options...)
-
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), options...)
-	defer cancel()
-
-	// create chrome instance
-	ctx, cancel := chromedp.NewContext(
-		allocCtx,
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
-
-	//建立一個上下文
-	// ctx, cancel = context.WithTimeout(ctx, 300*time.Second)
-	ctx, cancel = context.WithCancel(ctx)
-	defer cancel()
-
-	// ch := make(chan int32)
-
-	// go func(ctx context.Context, ch chan int32) {
-	//  ch <- 1
-	// }(ctx, ch)
-
-	// go func(ctx context.Context, ch chan int32) {
-	//  ctx.Done()
-	// }(ctx, ch)
-
-	// var count int32 = 0
-	// for {
-	//  select {
-	//  case value := <-ch:
-	//    count += value
-	//  case <-ctx.Done():
-	//    fmt.Println("finish")
-	//    break
-	//  }
-	// }
-
-	// spew.Dump(count)
-
+// 計算排名
+func computedRank(ctx context.Context, ch chan bool) {
+	var err error
 	Rank := 0 // 排名
 	hasCurrentItem := false
 	keyWord := "二手精品" // 搜尋關鍵字
@@ -117,6 +63,133 @@ func main() {
 
 		Rank, hasCurrentItem = ParsingData(htmlContent, Rank, i)
 		if hasCurrentItem {
+			ctx.Done()
+			return
+		}
+		ch <- false
+	}
+}
+
+func main() {
+	options := []chromedp.ExecAllocatorOption{
+		chromedp.ExecPath("/Users/ian/Desktop/Chromium.app/Contents/MacOS/Chromium"),
+		chromedp.Flag("no-default-browser-check", true),
+		chromedp.Flag("no-sandbox", true),
+		chromedp.Flag("blink-settings", "imagesEnabled=false"), // 不加載圖片
+		chromedp.Flag("headless", true),                        // 無頭模式
+		chromedp.WindowSize(1920, 1080),
+		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
+	}
+
+	dir, err := ioutil.TempDir("", "chromedp-example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	options = append(chromedp.DefaultExecAllocatorOptions[:], options...)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), options...)
+	defer cancel()
+
+	// create chrome instance
+	ctx, cancel := chromedp.NewContext(
+		allocCtx,
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
+
+	//建立一個上下文
+	// ctx, cancel = context.WithTimeout(ctx, 300*time.Second)
+	ctx, cancel = context.WithCancel(ctx)
+	defer cancel()
+
+	// ch := make(chan int32)
+
+	// go func(ctx context.Context, ch chan int32) {
+	// 	ch <- 1
+	// }(ctx, ch)
+
+	// go func(ctx context.Context, ch chan int32) {
+	// 	ctx.Done()
+	// }(ctx, ch)
+
+	// var count int32 = 0
+	// for {
+	// 	select {
+	// 	case value := <-ch:
+	// 		count += value
+	// 	case <-ctx.Done():
+	// 		fmt.Println("finish")
+	// 		break
+	// 	}
+	// }
+
+	// spew.Dump(count)
+
+	ch := make(chan string)
+	Rank := 0 // 排名
+	hasCurrentItem := false
+	keyWord := "二手精品" // 搜尋關鍵字
+
+	for i := 1; i <= 10; i++ {
+		time.Sleep(time.Second)
+		go func(i int) {
+			fmt.Printf("now in Page: %d\n", i)
+			// navigate
+			err = chromedp.Run(ctx,
+				chromedp.Navigate("https://www.google.com.tw/search?q="+keyWord+"&start="+strconv.Itoa(((i-1)*10))),
+				// chromedp.Sleep(1*time.Second),
+				chromedp.WaitReady(`#search div[id="rso"]`),
+			)
+			// if i == 1 {
+			// 	err = chromedp.Run(ctx,
+			// 		chromedp.Navigate("https://www.google.com.tw/search?q="+keyWord+"&start="+((i-1)*10)),
+			// 		// chromedp.Sleep(1*time.Second),
+			// 		chromedp.WaitReady(`#search div[id="rso"]`),
+			// 	)
+			// } else {
+			// 	pageSelection := `a[aria-label="Page ` + strconv.Itoa(i) + `"]`
+			// 	err = chromedp.Run(ctx,
+			// 		chromedp.Click(pageSelection),
+			// 		// chromedp.Sleep(1*time.Second),
+			// 		chromedp.WaitReady(`#search div[id="rso"]`),
+			// 	)
+			// }
+			if err != nil {
+				logger.Info("Run err :", err)
+				return
+			}
+
+			// outer source data
+			htmlContent, runErr := outerPageSourceData(ctx)
+			if runErr != nil {
+				logger.Info("Outer err : %v", runErr)
+				return
+			}
+
+			sleepErr := chromedp.Run(ctx,
+				chromedp.Sleep(2*time.Second),
+			)
+			if sleepErr != nil {
+				log.Fatal(sleepErr)
+			}
+
+			Rank, hasCurrentItem = ParsingData(htmlContent, Rank, i)
+			if hasCurrentItem {
+				ctx.Done()
+				return
+			}
+			ch <- "Not Found"
+		}(i)
+	}
+
+	for {
+		select {
+		case v := <-ch:
+			fmt.Println(v)
+		case <-ctx.Done():
+			fmt.Println("finish")
 			return
 		}
 	}
